cdf3f4d35b84dd41ffe25cdd16d73e8a4d311608
Xandekkj <alexandre_rodrigues2003@hotmail.com>
2023-08-11 15:56:17 -0300

    som-em-todos-botões

--- JS/main.js
+++ JS/main.js
@@ -2,4 +2,12 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_pom').play()
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = tocaSomPom
+
+    contador = contador + 1
+}
