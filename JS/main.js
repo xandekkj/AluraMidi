5bbbcf14736ddd3c0fc81df53f5462444358d8fa
Xandekkj <alexandre_rodrigues2003@hotmail.com>
2023-08-11 16:19:14 -0300

    mudando-para-for

--- JS/main.js
+++ JS/main.js
@@ -4,9 +4,7 @@ function tocaSom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
     const instrumento = listaDeTeclas[contador].classList[1];
@@ -15,6 +13,4 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
-
-    contador = contador + 1
 }
